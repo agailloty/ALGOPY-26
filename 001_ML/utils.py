e59b230726605dbc2310b6b911db33064b20a4b5
agailloty <mr.clerisaxel@hotmail.fr>
2026-02-06 09:29:52 +0100

    extraire le contenu du zip

--- 001_ML/utils.py
+++ 001_ML/utils.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import requests
 import os
+import zipfile
 
 
 download_folder = Path(".") / "dataset"
@@ -16,3 +17,9 @@ def download_dataset():
     if response.status_code == 200:
         with open(file_path, "wb") as file:
             file.write(response.content)
+        print("Fichier correctement téléchargé")
+    else:
+        print("Erreur au téléchargement")
+
+    with zipfile.ZipFile(file_path, "r") as zip_ref:
+        zip_ref.extractall(download_folder)
