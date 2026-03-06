868f4b05c60bb85801783c2b0132b7a33070f4d7
agailloty <mr.clerisaxel@hotmail.fr>
2026-03-06 09:59:03 +0100

    webscraping advanced

---
+++ 003__webscraping_advanced/Programme.md
@@ -0,0 +1,5 @@
+# Enregistrer 5 pages sofifa dans un dossier data
+
+# Extraire le contenu avec des requêtes xpath
+
+# Enregistrer ce contenu dans un format csv
--- 003__webscraping_advanced/page_saver.py
+++ 003__webscraping_advanced/page_saver.py
@@ -1,5 +1,11 @@
+# Script pour enregistrer les pages HTML en provenance de sofifa.com
+
+# J'utilise playwright codegen sofifa.com pour générer du script Python
+# Que je vais adapter
+
 import re
 from playwright.sync_api import Playwright, sync_playwright, expect
+import time
 
 
 def run(playwright: Playwright) -> None:
@@ -7,15 +13,23 @@ def run(playwright: Playwright) -> None:
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://sofifa.com/")
+
     page.get_by_role("button", name="Consent", exact=True).click()
+
+    # Sélectionner deux colonnes supplémentaires : Height et Weight
     page.get_by_role("textbox", name="Add column").click()
-    page.get_by_role("treeitem", name="Birth year").click()
     page.get_by_role("treeitem", name="Height").click()
     page.get_by_role("treeitem", name="Weight").click()
     page.get_by_role("main").filter(has_text="Columns selected AgeOverall").click()
     page.get_by_role("button", name="Apply").click()
-    page.get_by_role("link", name="Next").click()
 
+    # Boucler pour sauvegarder les pages.
+
+    for i in range(5):
+        with open(f"sofifa-page-{i+1}.html", "w", encoding="utf-8") as file: # r : read ; w: write
+            file.write(page.content())
+        page.get_by_role("link", name="Next").click()
+        time.sleep(2)
     # ---------------------
     context.close()
     browser.close()
