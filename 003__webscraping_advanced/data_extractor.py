7cd16b912979e0732d88a7716e6bc12386ed7274
agailloty <mr.clerisaxel@hotmail.fr>
2026-03-13 11:17:26 +0100

    finalisation projet webscraping

--- 003__webscraping_advanced/data_extractor.py
+++ 003__webscraping_advanced/data_extractor.py
@@ -12,11 +12,33 @@ with open("data/sofifa-page-1.html", "r", encoding="utf-8") as file:
 
 xml_page = fromstring(page1)
 
-names = xml_page.xpath("//a[starts-with(@href, '/player/') and @aria-expanded='false']/text()")
+# names = xml_page.xpath("//a[starts-with(@href, '/player/') and @aria-expanded='false']/text()")
 
-ages = xml_page.xpath("//td[@class='d2' and @data-col='ae']/text()")
+# ages = xml_page.xpath("//td[@class='d2' and @data-col='ae']/text()")
 
-teams = xml_page.xpath("//a[starts-with(@href, '/team/')]/text()")
+# teams = xml_page.xpath("//a[starts-with(@href, '/team/')]/text()")
 
 
-print(list(zip(names, ages, teams)))
+# print(list(zip(names, ages, teams)))
+
+players = xml_page.xpath('//a[starts-with(@href, "/player/") and @data-tippy-top="" and @aria-expanded="false"]/text()')
+ages = xml_page.xpath('//td[@class="d2" and @data-col="ae"]/text()')
+values = xml_page.xpath('//td[@class="d6" and @data-col="vl"]/text()')
+wages = xml_page.xpath('//td[@class="d6" and @data-col="wg"]/text()')
+
+
+#print(list(zip(players, ages)))
+
+import pandas as pd
+
+data = {
+    "names" : players,
+    "ages" : ages,
+    "values" : values,
+    "wages" : wages
+}
+
+df = pd.DataFrame(data)
+
+df.to_csv("data_extracted.csv", sep = ",")
+
